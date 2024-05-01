bd8213bbc142ee6323ae28cd4a04d77c2f74da0e
Nhan Lam <lamnhan92@gmail.com>
2024-05-01 20:56:36 +0700

    feat: update v0.18.0

--- cli/expand.ts
+++ cli/expand.ts
@@ -1,10 +1,13 @@
 import {defineTiniCLIExpansion, resolveCommand} from '@tinijs/cli';
 
-export default defineTiniCLIExpansion({
+export type MyCLIExpansionOptions = {};
+
+export default defineTiniCLIExpansion<MyCLIExpansionOptions>({
   meta: {
     name: 'my-cli',
+    url: 'https://tinijs.dev/cli', // optional usage url
   },
-  setup() {
+  setup(options, tini) {
     // register the list of expandable commands
     return {
       xxx: () => import('./commands/xxx.js').then(resolveCommand),
